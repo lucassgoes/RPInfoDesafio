5db840ccf4b33afcc004fb33936385fadbb500c0
lucassgoes <lucas-goes@hotmail.com>
2023-01-19 13:38:10 -0300

    prints adicionados para o readme

--- README.md
+++ README.md
@@ -3,5 +3,28 @@
 Aplicação REST para registro de ordem de serviço utilizando Spring boot.
 
 ## Como rodar a Aplicação
-Só clonar o repositório e exportar para a IDE como projeto Maven depois rodar a classe RpInfoApiApplication e acessar [http://localhost:8080](http://localhost:8080) no navegador.
+- Passo 1: Clone o repositório
+```
+git clone https://github.com/lucassgoes/RPInfoDesafio.git
+```
+- Passo 2: Abra o projeto na sua IDE e rode a classe RpInfoDesafioApplication
+- Passo 3: Acesse http://localhost:8080 no seu navegador.
 
+## Documentação API Rest
+### Métodos
+Requisições para a API devem seguir os padrões:
+| Método | Descrição |
+|---|---|
+| `GET` | Retorna informações de um ou mais ordens de serviço. |
+| `POST` | Utilizado para criar uma nova ordem de serviço. |
+| `PUT` | Atualiza dados de uma ordem de serviço e alterar sua situação. |
+| `DELETE` | Remove uma ordem de serviço do sistema. |
+
+### Rotas
+Requisições para a API devem seguir os padrões:
+| Método | Rotas |
+|---|---|
+| `GET` | /api/workOrders |
+| `POST` | /api/workOrders | 
+| `PUT` | /api/workOrders/{id} |
+| `DELETE` | /api/workOrders/{id} |
--- src/main/java/com/lucas/RPInfoDesafio/InitialValueDb.java
+++ src/main/java/com/lucas/RPInfoDesafio/InitialValueDb.java
@@ -21,7 +21,7 @@ public class InitialValueDb implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		WorkOrder w1 = new WorkOrder("Mario");
 		w1.setClientAddress("Rua Ipê");
-		w1.setClientContact("419877654");
+		w1.setClientContact("41987762354");
 		w1.setClientEmail("mario@gmail.com");
 		w1.setEquipamentBrand("Samsung");
 		w1.setEquipamentModel("Galaxy");
@@ -29,6 +29,28 @@ public class InitialValueDb implements CommandLineRunner{
 		w1.setWorkOrderStatus(workOrderStatus.ONGOING);
 		
 		workOrderRepository.save(w1);
+		
+		WorkOrder w2 = new WorkOrder("Suzana");
+		w2.setClientAddress("Rua Diamantina");
+		w2.setClientContact("72986523456");
+		w2.setClientEmail("Suzana@gmail.com");
+		w2.setEquipamentBrand("Motorola");
+		w2.setEquipamentModel("Moto G");
+		w2.setEquipamentProblem("Não Liga");
+		w2.setWorkOrderStatus(workOrderStatus.OPEN);
+		
+		workOrderRepository.save(w2);
+		
+		WorkOrder w3 = new WorkOrder("Joana");
+		w3.setClientAddress("Rua Marechal");
+		w3.setClientContact("44987451236");
+		w3.setClientEmail("Joana@gmail.com");
+		w3.setEquipamentBrand("Apple");
+		w3.setEquipamentModel("Iphone X");
+		w3.setEquipamentProblem("Lento");
+		w3.setWorkOrderStatus(workOrderStatus.CLOSED);
+		
+		workOrderRepository.save(w3);
 	}
 
 }
